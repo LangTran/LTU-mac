dce5eec113145c8916cda5766ef1fffac3abd56d
Jim Henderson <jim.henderson44@gmail.com>
2019-12-13 17:21:34 +1100

    Installer now puts script in Applications and the rest in /Users/Shared.

--- LangTranLocal/Progs/LT-setup.sh
+++ LangTranLocal/Progs/LT-setup.sh
@@ -38,7 +38,7 @@ case `uname` in
 	dscl . list /Groups | grep $$LTgroup >/dev/null ||
 	{
 	    LTtopGpNo=`dscl . list /Groups PrimaryGroupID |
-		sort -n +1 | sed -n -e 's/[^0-9]*//' -e '$$p'`
+		sort -n +1 | awk '{LastNo = $$NF}; END {print LastNo}'`
 	    LTdsclErrors=`expr $$LTdsclErrors + $$?`
 	    echo Top group number is $$LTtopGpNo
 	    LTnewGpNo=`expr $$LTtopGpNo + 1`
